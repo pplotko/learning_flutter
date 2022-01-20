856a18a6a613fe8c35a8d2403606ce290f15a1a9
Pavel Platko <94006710+pplotko@users.noreply.github.com>
2022-01-20 17:38:40 +0300

    Add task_1_container files

--- main.dart
+++ main.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:task_1_container/circular_container.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'task_1_container',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TaskPage(),
+    );
+  }
+}
+
+class TaskPage extends StatelessWidget {
+  const TaskPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: circularContainer(),
+      ),
+    );
+  }
+}
